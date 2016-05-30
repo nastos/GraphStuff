e3d2f5809c5be3efa903b46cab54df121ac6cf08
nastos <nastos@gmail.com>
2016-05-30 13:52:34 -0700

    Add files via upload

---
+++ Recognition.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import org.jgrapht.Graphs;
+//import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class Recognition {
+
+	public static void main(String[] args) {
+        SimpleGraph<Integer, DefaultEdge>  g = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class); 
+	}
+
+	public static <V, E> int[] degSeq(SimpleGraph<V,E> g) {
+		// gives int array sorted by natural order of vertex order, if one exists.
+		int n = g.vertexSet().size();
+		int[] s = new int[n];
+		int i=0;
+		for (V v : g.vertexSet()) {
+			s[i] = g.degreeOf(v);
+			i++;
+		}
+		return s;
+	}
+
+	public static <V,E> boolean isSimplicial(SimpleGraph<V,E> g, V v) {
+		ArrayList<V> nbrs = (ArrayList<V>) Graphs.neighborListOf(g, v);
+				
+		for (int a=0; a<nbrs.size(); a++) {
+			for (int b=a+1; b<nbrs.size(); b++) {
+				if (g.containsEdge(nbrs.get(a), nbrs.get(b)) == false) return false;
+			}
+		}
+		return true;
+	}
+	
+	
+}
