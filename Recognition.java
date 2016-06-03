3c48ab1eb139fbb9624f80f959dac96867624cb3
nastos <nastos@gmail.com>
2016-06-03 12:58:04 -0700

    Add files via upload
    added isTwoPair

--- Recognition.java
+++ Recognition.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.DijkstraShortestPath;
 //import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
@@ -82,7 +84,18 @@ public class Recognition {
 	public static <V,E> SimpleGraph<V,E> copy (SimpleGraph<V,E> g) {
 		return (SimpleGraph<V, E>) g.clone();
 	}
-		
+	
+	public static <V,E> boolean isTwoPair(SimpleGraph<V,E> g, V a, V b) {
+		if (g.containsEdge(a,b) == true) return false;
+		SimpleGraph<V,E> g2 = (SimpleGraph<V, E>) g.clone();
+		HashSet<V> toDelete = new HashSet<V>(Graphs.neighborListOf(g2,a));
+		toDelete.retainAll(Graphs.neighborListOf(g2, b));
+		g2.removeAllVertices(toDelete);
+		DijkstraShortestPath d = new DijkstraShortestPath(g2,a,b);
+		if (d.getPathEdgeList() == null) return true;
+		return false;
+	}
+	
 
 
 	
